7f08f5f4f7a162c299d9305d23f93e111b8ad07f
AIMAD KACEM <kacem.aimad@gmail.com>
2024-08-06 00:06:56 +0100

    4-tasks.py

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""multiple coroutines module"""
+
+import asyncio
+import random
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """return the list of all the delays"""
+    random_delays: List[float] = []
+    list_delays: List[float] = []
+
+    for _ in range(n):
+        random_delays.append(
+            task_wait_random(max_delay)
+        )
+    for done in asyncio.as_completed(random_delays):
+        result = await done
+        list_delays.append(result)
+    return list_delays
