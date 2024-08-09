f7e95fd9f9ee5e047af73e42b8f031cfca6149b7
AIMAD KACEM <kacem.aimad@gmail.com>
2024-08-10 00:37:43 +0100

    import Any

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """duck typing annotations Module"""
-from typing import Union, Sequence
+from typing import Union, Sequence, Any
 
 
-def safe_first_element(lst: Sequence[any]) -> Union[any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """safe first element"""
     if lst:
         return lst[0]
