361844934e7bc37f9c1f317cf513e23fa3447076
AIMAD KACEM <kacem.aimad@gmail.com>
2024-08-09 23:49:04 +0100

    use union

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """list of mixed types module"""
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """returm sum of mixed list items"""
     sum: float = 0.0
     for item in mxd_lst:
