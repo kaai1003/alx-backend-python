513c4a9e77c5453313bd2df331ef55c85112480e
AIMAD KACEM <kacem.aimad@gmail.com>
2024-08-09 23:43:40 +0100

    6-sum_mixed_list.py

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""list of mixed types module"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+    """returm sum of mixed list items"""
+    sum: float = 0.0
+    for item in mxd_lst:
+        sum += item
+    return sum
