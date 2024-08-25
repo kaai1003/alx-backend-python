76855235de73c05519f51281073e39de90b168ff
AIMAD KACEM <kacem.aimad@gmail.com>
2024-08-25 16:54:07 +0100

    test_utils.py

---
+++ 0x03-Unittests_and_integration_tests/test_nested_map.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""test nested map func"""
+
+access_nested_map = __import__('utils').access_nested_map
+
+nested_map={"a": 1}
+path=("a",)
+print(access_nested_map(nested_map, path))
+nested_map={"a": {"b": 2}}
+path=("a",)
+print(access_nested_map(nested_map, path))
+nested_map={"a": {"b": 2}}
+path=("a", "b")
+print(access_nested_map(nested_map, path))
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""TestAccessNestedMap class Module"""
+from parameterized import parameterized
+import unittest
+
+access_nested_map = __import__('utils').access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """TestAccessNestedMap class definition"""
+    @parameterized.expand([({"a": 1}, ("a",), 1),
+                           ({"a": {"b": 2}}, ("a",), {'b': 2}),
+                           ({"a": {"b": 2}}, ("a", "b"), 2)])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """test access_nested_map func"""
+        test = access_nested_map(nested_map, path)
+        self.assertEqual(test, expected)
